e8a6836e2f88c7354c1cc9f50c3e20d78792c3ec
Willistic <jlubbick@aol.com>
2022-04-14 10:43:44 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [isSubmitted, setIsSubmitted] = useState(false)
   const [items, setItems] = useState("")
 
-  function Button({ setItems, number }) {
+  function Button({ number }) {
     
     return (
         <button onClick={() => setItems(number)} className="btn-number bg-gray-700 h-10 w-10 rounded-full text-gray-200 pt-1 focus:bg-gray-400 transition-all duration-150">{ number }</button>
